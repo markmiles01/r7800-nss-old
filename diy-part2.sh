2bae5764b7f4ebde4aabf9422dc0618ef935e36f
mapdio <77219481+mapdio@users.noreply.github.com>
2021-01-25 03:44:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#自定义设置
-
-        [ -e $DEF_SETS ] && mv -f $DEF_SETS package/lean/default-settings/files/
-
 
 
 
