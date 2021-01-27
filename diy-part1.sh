f6b41f17b86de90faa513a3a9d0b03166f1b32b9
mapdio <77219481+mapdio@users.noreply.github.com>
2021-01-27 19:09:55 +0800

    Update diy-part1.sh
    恢复kenzok8 插件包，移除重复组件

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
 # 移除ACwifidude自带bin目录
 rm -rf bin
 
@@ -25,23 +26,32 @@ cp -r lede/package/lean package/
 
 
 # 添加 ssr plus 和passwall 支持源
-# sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small  https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small  https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git OpenAppFilter https://github.com/OpenWrt-Actions/OpenAppFilter' feeds.conf.default
-sed -i '/helloworld/ s/#//'  feeds.conf.default
 
 
-#添加UPX UCL工具包
+
+# 添加UPX UCL工具包
 cp -r lede/tools/upx tools
 cp -r lede/tools/ucl tools
 
-#修改makefile
+# 修改makefile
 sed  -i '/^# builddir dependencies/i\tools-y += ucl upx' ./tools/Makefile
 sed  -i '/^# builddir dependencies/a\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
 
-#删除lede，lineol文件夹
+
+# 删除重复插件
+rm -rf package/lean/ipt2socks
+rm -rf package/lean/dns2socks
+rm -rf package/lean/pdnsd-alt
+rm -rf package/lean/shadowsocksr-libev
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/v2ray-plugin
+rm -rf package/lean/v2ray
+rm -rf package/lean/microsocks
+
+# 删除lede文件夹
 rm -rf lede
-
-
 
 
